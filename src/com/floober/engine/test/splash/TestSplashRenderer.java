be43fda0bb1661b29b6d0ef57fb9fe4dbd09d39b
Garrett Nix <51683504+GarrettNixSoft@users.noreply.github.com>
2022-06-11 18:37:26 -0700

    Modified the sample splash renderer to scale with fake latency time

--- src/com/floober/engine/test/splash/TestSplashRenderer.java
+++ src/com/floober/engine/test/splash/TestSplashRenderer.java
@@ -37,7 +37,7 @@ public class TestSplashRenderer extends SplashRenderer {
 	public void render() {
 		loadText.replaceText(GameLoader.getLoadString());
 
-		float size = Config.SPLASH_WIDTH * timer.getTimeElapsedSeconds() / 5;
+		float size = Config.SPLASH_WIDTH * timer.getTimeElapsedSeconds() / Config.SPLASH_FAKE_LATENCY;
 		RectElement redRect = new RectElement(Colors.RED, 0, Config.SPLASH_HEIGHT - 50, Layers.BOTTOM_LAYER + 1, size, 50, false);
 		redRect.transform(SplashScreen.splashWindow);
 		Render.drawRect(redRect);
