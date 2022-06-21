2a4e166c2e020ba7c6654f4d45c2dd6ad35e153a
Garrett Nix <51683504+GarrettNixSoft@users.noreply.github.com>
2022-06-20 20:42:27 -0700

    Moved the dialogue package back to the DDPC project

--- src/com/floober/engine/gui/dialogue/DialogueConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.floober.engine.gui.dialogue;
-
-import com.floober.engine.core.renderEngine.fonts.fontMeshCreator.FontType;
-import org.joml.Vector4f;
-
-public record DialogueConfiguration(FontType defaultTextFont, FontType defaultNameFont, Vector4f defaultTextColor,
-									Vector4f defaultNameColor, Vector4f defaultBackgroundColor,
-									float defaultTextSize, float defaultNameSize) {}
