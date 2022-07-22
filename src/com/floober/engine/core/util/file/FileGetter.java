2eb2afc748ff7509a40f36d143ca2f9424707565
Garrett Nix <51683504+GarrettNixSoft@users.noreply.github.com>
2022-07-21 20:48:48 -0700

    Added a utility function to FileGetter for choosing a directory only.
    FileGetter now shows directories in addition to any file extension filters when calling getFile().

--- src/com/floober/engine/core/util/file/FileGetter.java
+++ src/com/floober/engine/core/util/file/FileGetter.java
@@ -12,12 +12,12 @@ public class FileGetter {
 
 		JFileChooser fileChooser = new JFileChooser(startingDir);
 
-
 		FileFilter fileFilter = new FileFilter() {
 			@Override
 			public boolean accept(File f) {
 				for (String ext : extensions) {
 					if (f.getName().toLowerCase().endsWith(ext)) return true;
+					else if (f.isDirectory()) return true;
 				}
 				return false;
 			}
@@ -32,6 +32,30 @@ public class FileGetter {
 		fileChooser.setFileFilter(fileFilter);
 
 		JFrame parent = new JFrame("Choose a Map File");
+		parent.setIconImage(Config.ICON_IMAGE);
+		parent.setAlwaysOnTop(true);
+
+		int selection = fileChooser.showOpenDialog(parent);
+		File selectedFile = fileChooser.getSelectedFile();
+
+		parent.dispose();
+
+		if (selection == JFileChooser.APPROVE_OPTION)
+			return selectedFile;
+
+		else if (selection == JFileChooser.CANCEL_OPTION)
+			return null;
+
+		else return null;
+
+	}
+
+	public static File getDirectory(String startingDir) {
+
+		JFileChooser fileChooser = new JFileChooser(startingDir);
+		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+		JFrame parent = new JFrame("Choose a Folder");
 		parent.setIconImage(Config.ICON_IMAGE);
 		parent.setAlwaysOnTop(true);
 
