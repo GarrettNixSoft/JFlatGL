13941c9f2d78021dc2f015c7162e70d2a51dd010
Garrett Nix <51683504+GarrettNixSoft@users.noreply.github.com>
2022-08-04 18:49:48 -0700

    Created a basic wrapper API for using the JOptionPane to get user input and show messages.

---
+++ src/com/floober/engine/core/util/input/UserInput.java
@@ -0,0 +1,49 @@
+package com.floober.engine.core.util.input;
+
+import javax.swing.*;
+
+public class UserInput {
+
+	private static final JFrame frame = new JFrame();
+
+	static {
+		frame.setAlwaysOnTop(true);
+	}
+
+	/**
+	 * Prompt the user to enter a String of input.
+	 * @param prompt the prompt message to show the user
+	 * @return the user's input as a String
+	 */
+	public static String getUserString(String prompt) {
+		return JOptionPane.showInputDialog(frame, prompt);
+	}
+
+	/**
+	 * Prompt the user to enter an integer as input.
+	 * Will repeat each time the user enters a value that
+	 * causes {@code Integer.parseInt()} to fail. If the
+	 * user chooses to cancel, 0 will be returned.
+	 * @param prompt the prompt message to show the user
+	 * @return the user's input as an int
+	 */
+	public static int getUserInt(String prompt) {
+		while (true) {
+			String response = getUserString(prompt);
+
+			if (response == null) return 0;
+
+			try {
+				return Integer.parseInt(response);
+			}
+			catch (NumberFormatException e) {
+				showErrorMessage("Please enter a valid integer.", "Invalid Input");
+			}
+		}
+	}
+
+	public static void showErrorMessage(String message, String title) {
+		JOptionPane.showMessageDialog(frame, message, title, JOptionPane.ERROR_MESSAGE);
+	}
+
+}
