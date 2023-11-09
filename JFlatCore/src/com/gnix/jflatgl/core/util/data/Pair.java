cd43b2dd06db9d0a2d0d5a724f50e307fc7edc7a
Garrett Nix <51683504+GarrettNixSoft@users.noreply.github.com>
2023-11-08 22:10:12 -0800

    Added method to compare Pairs

--- JFlatCore/src/com/gnix/jflatgl/core/util/data/Pair.java
+++ JFlatCore/src/com/gnix/jflatgl/core/util/data/Pair.java
@@ -27,6 +27,14 @@ public record Pair<T1, T2>(T1 data1, T2 data2) {
 
 	@Override
 	public String toString() {
-		return String.format("Pair<%s, %s>(%s, %s)", data1.getClass().getName(), data2.getClass().getName(), data1, data2);
+		return String.format("Pair(%s,%s)", data1, data2);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Pair<?,?> p) {
+			return data1.equals(p.data1) && data2.equals(p.data2);
+		}
+		else return false;
 	}
 }
